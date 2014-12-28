7af26f7ba4bab7f7d3169ee21bde11167486b039
Andrej Petras <andrej@ajka-andrej.com>
2014-12-28 10:20:33 +0100

    Create ajax status dialog.

--- tower-web/src/main/resources/META-INF/resources/beefaces/js/beefaces.js
+++ tower-web/src/main/resources/META-INF/resources/beefaces/js/beefaces.js
@@ -1,15 +1,46 @@
-function Bf() {}
-Bf.id = function (a) { return"#" + a.replace(/:/g, "\\:"); };
-Bf.e = function(a) { return $(Bf.id(a)); };
-Bf.filter = function(a) {var i = a + ":input-filter";Bf.e(i).filterTable(a + ":content:table");Bf.e(a + ":clear-filter").clearTable(i);};
+function Bf() {
+}
+Bf.id = function (a) {
+    return"#" + a.replace(/:/g, "\\:");
+};
+Bf.e = function (a) {
+    return $(Bf.id(a));
+};
+Bf.filter = function (a) {
+    var i = a + ":input-filter";
+    Bf.e(i).filterTable(a + ":content:table");
+    Bf.e(a + ":clear-filter").clearTable(i);
+};
 
-Bf.spin = function(data) {
+Bf.spin = function (data) {
     var status = data.status;
     if (status === "begin") {
         Bf.e(data.source.id).find('i').addClass('fa-spin');
     } else if (status === "success") {
         Bf.e(data.source.id).find('i').removeClass('fa-spin');
     }
+};
+
+Bf.status = function (i, t) {
+    var loading;
+    if (!window["bs"]) {
+        var bs = {};
+    }
+    bs.onStatusChange = function (data) {
+        var status = data.status;
+        if (status === "begin") {
+            loading = setTimeout(function () {
+                document.body.style.cursor = 'wait';
+                Bf.e(i).modal('show');
+            }, t);
+        }
+        else if (status === "success") {
+            clearTimeout(loading);
+            Bf.e(i).modal('hide');
+            document.body.style.cursor = 'auto';
+        }
+    };
+    jsf.ajax.addOnEvent(bs.onStatusChange);
 };
 
 (function () {
@@ -31,7 +62,7 @@ Bf.spin = function(data) {
                     var $this = $(this), search = $this.val().toLowerCase();
                     var target = Bf.id(x);
                     var $target = $(target), $rows = $target.find('tbody:first tr');
-                    $target.find('tbody:last').hide();                    
+                    $target.find('tbody:last').hide();
                     $target.find('thead').show();
                     if (search === '') {
                         $rows.show();
